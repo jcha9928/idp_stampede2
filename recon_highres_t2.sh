93daa41412fdeba1ee7feb4b752c553f520f2fad
Jiook Cha <cha.jiook@gmail.com>
2017-09-12 14:37:42 -0400

    Update recon_highres_t2.sh

--- recon_highres_t2.sh
+++ recon_highres_t2.sh
@@ -5,7 +5,8 @@ s=10004953_20111116
 
 SUBJECTS_DIR=/ifs/scratch/pimri/posnerlab/1anal/IDP/fs
 
-IMPATH=/ifs/scratch/pimri/posnerlab/1anal/IDP/${year}/${s}
+#IMPATH=/ifs/scratch/pimri/posnerlab/1anal/IDP/${year}/${s}
+IMPATH=/ifs/scratch/pimri/posnerlab/1anal/IDP/data/${s}
 EXPERTOPT=$SUBJECTS_DIR/expert.opt
 FLAIR=`ls $IMPATH/flair*nii`
 T1=`ls $IMPATH/t1*nii`
@@ -20,7 +21,8 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 SUBJECTS_DIR=/ifs/scratch/pimri/posnerlab/1anal/IDP/fs
 echo NOW PERFORMING RECON-ALL
 #recon-all -all -s ${SUBJECT}.test_mpi128 -hires -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1 -openmp 64 
-recon-all -all -s ${SUBJECT}_1mm_mpi12 -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1T2 $FLAIR flair -openmp 12
+#recon-all -all -s ${SUBJECT}_1mm_mpi12 -i $T1 -expert $EXPERTOPT -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1T2 $FLAIR flair -openmp 12
+recon-all -all -s ${SUBJECT}_1mm_mpi12 -i $T1 -FLAIR $FLAIR -FLAIRpial -hippocampal-subfields-T1T2 $FLAIR flair -parallel -openmp 12
 EOC
 
 chmod +x $recon
@@ -30,7 +32,7 @@ cat<<-EOM >$CMD
 #!/bin/bash
 #$ -V
 #$ -cwd -S /bin/bash -N recon
-#$ -l mem=3G,time=48::
+#$ -l mem=1G,time=12::
 #$ -pe orte 12
 #$ -o /ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job -e /ifs/scratch/pimri/posnerlab/1anal/IDP/code/idp/job 
 #$ -l infiniband=TRUE
